a90f8f23b548f704212997c5f2e860287134f2f6
Никита Педич <pedichn@gmail.com>
2022-04-06 00:14:49 +0300

    Удалена вторая строка

--- ClassTotal.cs
+++ ClassTotal.cs
@@ -9,7 +9,6 @@ namespace LegoStore
 {
     public static class ClassTotal
     {
-        //public static string connectionString = @"Data Source=Laptop\SQLEXPRESS;Initial Catalog=LegoStore;Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";  //Строка подключения
         public static string connectionString = @"Data Source=LapTop, 1433;Initial Catalog=LegoStore;User ID=test;Password=test;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";  //Строка подключения
         public static SqlConnection connection;
         public static int id;
